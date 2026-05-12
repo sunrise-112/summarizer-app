4382d284524d676f3b1a4d28cc2263c8fc966a95
sunrise-112 <mohamedbouraada2000@gmail.com>
2026-05-12 05:34:03 +0100

    chore: initialize server and client projects with configs and migrations

--- packages/server/index.js
+++ packages/server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(helmet());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
